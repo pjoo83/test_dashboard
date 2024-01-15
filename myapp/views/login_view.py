85867fb565d190207bd4b599d10a6fbe9b8f7d4d
pjoo83 <pjoo83@163.com>
2024-01-15 17:40:10 +0800

    登陆部分

--- myapp/views/login_view.py
+++ myapp/views/login_view.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 class LoginForm(forms.Form):
@@ -19,5 +19,42 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user:
                 if user.is_staff:
-                    # 权限装饰器
-                    login(request,user)
+                    # 权限装饰器，判断是否有权限可进入项目
+                    login(request, user)
+                    # 存入session，超时需登陆
+                    request.session['info'] = {'id': request.POST.get('id'), 'name': username}
+                    request.session.set_expiry(60 * 60 * 24)
+                    return redirect('/autotest/index/')
+                else:
+                    return render(request, 'login.html',
+                                  {
+                                      "username": username,
+                                      "password": password,
+                                      "message": "请联系管理员开通权限"
+                                  })
+            else:
+                return render(request, 'login.html',
+                              {
+                                  "username": username,
+                                  "password": password,
+                                  "message": "用户名或密码错误，请检查后并重新输入"
+                              })
+        elif username == '':
+            return render(request, 'login.html',
+                          {
+                              "username": '',
+                              "password": '',
+                              "message": "请输入用户名"
+                          })
+        elif password == '':
+            return render(request, 'login.html',
+                          {
+                              "username": username,
+                              "password": "",
+                              "message": "请输入用户密码"
+                          })
+        else:
+            return render(request, 'register.html',
+                          {
+                              "message": "用户未注册，请注册"
+                          })
